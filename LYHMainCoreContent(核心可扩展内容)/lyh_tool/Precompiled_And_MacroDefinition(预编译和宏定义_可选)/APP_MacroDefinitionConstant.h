e88a3805f454c1192c99e5c86ef65f02471ec980
lee <lee@leedeMacBook-Pro.local>
2018-11-01 17:04:09 +0800

    项目的appdelegate瘦身、iPhone、iPad和状态栏、导航栏、标签栏的高度宏定义

---
+++ LYHMainCoreContent(核心可扩展内容)/lyh_tool/Precompiled_And_MacroDefinition(预编译和宏定义_可选)/APP_MacroDefinitionConstant.h
@@ -0,0 +1,42 @@
+//
+//  APP_MacroDefinitionConstant.h
+//  NTES_IM_CocoaPods
+//
+//  Created by lee on 2018/11/1.
+//  Copyright © 2018年 lee. All rights reserved.
+//
+
+#ifndef APP_MacroDefinitionConstant_h
+#define APP_MacroDefinitionConstant_h
+
+#pragma mark ------------------------ h5框架+文件加载路径__(可选) ------------------------
+
+#define K_H5App_BaseInit_Location_pWWWPath @"Pandora/apps/H55E48D08/www"    // www的路径
+#define K_H5App_BaseInit_Location_IndexPath @"form_mobile/index/foot.html"    // launch_path的路径
+
+#pragma mark ------------------------ iPhone、ipad屏幕比例 ------------------------
+//判断是否是ipad
+#define isPad ([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad)
+//判断iPhone4系列
+#define kiPhone4 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 960), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
+//判断iPhone5系列
+#define kiPhone5 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(640, 1136), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
+//判断iPhone6系列
+#define kiPhone6 ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(750, 1334), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
+//判断iphone6+系列
+#define kiPhone6Plus ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1242, 2208), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
+//判断iPhoneX
+#define IS_IPHONE_X ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
+//判断iPHoneXr
+#define IS_IPHONE_Xr ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(828, 1792), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
+//判断iPhoneXs
+#define IS_IPHONE_Xs ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1125, 2436), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
+//判断iPhoneXs Max
+#define IS_IPHONE_Xs_Max ([UIScreen instancesRespondToSelector:@selector(currentMode)] ? CGSizeEqualToSize(CGSizeMake(1242, 2688), [[UIScreen mainScreen] currentMode].size) && !isPad : NO)
+
+#pragma mark ------------------------ iPhone、ipad 状态栏、导航栏、标签栏高度 ------------------------
+//iPhoneX系列
+#define Height_StatusBar ((IS_IPHONE_X==YES || IS_IPHONE_Xr ==YES || IS_IPHONE_Xs== YES || IS_IPHONE_Xs_Max== YES) ? 44.0 : 20.0)
+#define Height_NavBar ((IS_IPHONE_X==YES || IS_IPHONE_Xr ==YES || IS_IPHONE_Xs== YES || IS_IPHONE_Xs_Max== YES) ? 88.0 : 64.0)
+#define Height_TabBar ((IS_IPHONE_X==YES || IS_IPHONE_Xr ==YES || IS_IPHONE_Xs== YES || IS_IPHONE_Xs_Max== YES) ? 83.0 : 49.0)
+#endif /* APP_MacroDefinitionConstant_h */
